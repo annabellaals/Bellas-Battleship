fe04aafd81558b1400ae421d27da012474e1d323
annabellaals <160537930+annabellaals@users.noreply.github.com>
2024-05-20 09:53:29 +0500

    v2
    OOP implemented

--- run.py
+++ run.py
@@ -1,16 +1,28 @@
 import os, types
 from typing import List
 
-# Function to display the grid
-def display_grid(grid: List[List[str]]):
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            print(grid[i][j], end="\t")
-        print('\n')
+# Battleship class
+class Battleship:
+    def __init__(self, size) -> None:
+        self.grid = [[f"X"] * size] * size # Create the grid as an array
 
-# Get custom grid size
-grid_size = int(input("Enter grid size: "))
+    # Function to display the grid
+    def display(self):
+        # Check os type and clear terminal 
+        if "nt" in os.name:
+            os.system('cls')
+        else:
+            os.system('clear')
 
-grid = [[f"X"] * grid_size] * grid_size # Create the grid as an array
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                print(self.grid[i][j], end="   ")
+            print()
 
-display_grid(grid)
+if __name__ == "__main__":
+    # Get custom grid size
+    grid_size = int(input("Enter grid size: "))
+
+    # Play game
+    battleship = Battleship(grid_size)
+    battleship.display()
