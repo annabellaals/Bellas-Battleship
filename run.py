0397c119afec6b9b574efa43a00410b9987df4aa
annabellaals <160537930+annabellaals@users.noreply.github.com>
2024-05-20 09:39:09 +0500

    v1
    Grid creation and display

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os, types
+from typing import List
+
+# Function to display the grid
+def display_grid(grid: List[List[str]]):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            print(grid[i][j], end="\t")
+        print('\n')
+
+# Get custom grid size
+grid_size = int(input("Enter grid size: "))
+
+grid = [[f"X"] * grid_size] * grid_size # Create the grid as an array
+
+display_grid(grid)
