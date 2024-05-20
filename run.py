5e3e40a580882bddef0bcb2c0c12f98d790f575d
annabellaals <160537930+annabellaals@users.noreply.github.com>
2024-05-20 10:19:43 +0500

    v3
    display complete

--- run.py
+++ run.py
@@ -3,26 +3,59 @@ from typing import List
 
 # Battleship class
 class Battleship:
-    def __init__(self, size) -> None:
-        self.grid = [[f"X"] * size] * size # Create the grid as an array
+    def __init__(self, size, name) -> None:
+        self.player_name = name
 
-    # Function to display the grid
-    def display(self):
-        # Check os type and clear terminal 
+        # Variables denoting the characters
+        self.ship = "@"
+        self.target = "."
+        self.bomb = "X"
+
+        self.player_grid = [[self.target] * size] * size # Create the target grid as an array
+        self.computer_grid = [[self.target] * size] * size # Create the target grid as an array
+
+        self.clear()
+
+    # Function to clear the terminal
+    def clear(self):
+        # Checks the os type and clears terminal accordingly
         if "nt" in os.name:
             os.system('cls')
         else:
             os.system('clear')
 
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[i])):
-                print(self.grid[i][j], end="   ")
+    # Function to display the grid
+    def display(self):
+        self.display_player()
+        print()
+        self.display_computer()
+
+    # Player's grid display function
+    def display_player(self):
+        print(f"{self.player_name}'s board: ")
+        
+        for i in range(len(self.player_grid)):
+            for j in range(len(self.player_grid[i])):
+                print(self.player_grid[i][j], end="   ")
+            print()
+
+    # Computers's grid display function
+    def display_computer(self):
+        print("Computer's board: ")
+
+        for i in range(len(self.computer_grid)):
+            for j in range(len(self.computer_grid[i])):
+                if self.player_grid[i][j] == self.bomb:
+                    print(self.bomb, end="   ")
+                else:
+                    print(self.target, end="   ")
             print()
 
 if __name__ == "__main__":
     # Get custom grid size
     grid_size = int(input("Enter grid size: "))
+    name = input("Enter player name: ")
 
     # Play game
-    battleship = Battleship(grid_size)
+    battleship = Battleship(grid_size, name)
     battleship.display()
