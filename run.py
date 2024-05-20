edf345b813020e6cfd215b15d48392a1399d0596
annabellaals <160537930+annabellaals@users.noreply.github.com>
2024-05-20 12:28:05 +0500

    v4
    added a game loop to handle the main functionalities.. implemented user input for player coordinates and random generation for computer

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import os, types
+import os, types, random
 from typing import List
 
 # Battleship class
@@ -11,10 +11,32 @@ class Battleship:
         self.target = "."
         self.bomb = "X"
 
-        self.player_grid = [[self.target] * size] * size # Create the target grid as an array
-        self.computer_grid = [[self.target] * size] * size # Create the target grid as an array
+        self.size = size
+        self.player_grid = [[self.target for _ in range(size)] for _ in range(size)]  # Create the target grid as an array
+        self.computer_grid = [[self.target for _ in range(size)] for _ in range(size)]  # Create the target grid as an array
 
+        self.player_ship_count = 0
+        self.computer_ship_count = 0
+
+        self.create_ships()
         self.clear()
+
+    def create_ships(self):
+        while self.player_ship_count + 1 < self.size:
+            row = random.randint(0, self.size - 1)
+            col = random.randint(0, self.size - 1)
+
+            if self.player_grid[row][col] == self.target:
+                self.player_grid[row][col] = self.ship
+                self.player_ship_count += 1
+
+        while self.computer_ship_count + 1 < self.size:
+            row = random.randint(0, self.size - 1)
+            col = random.randint(0, self.size - 1)
+
+            if self.computer_grid[row][col] == self.target:
+                self.computer_grid[row][col] = self.ship
+                self.computer_ship_count += 1
 
     # Function to clear the terminal
     def clear(self):
@@ -23,6 +45,30 @@ class Battleship:
             os.system('cls')
         else:
             os.system('clear')
+
+    # Main gameloop
+    def play(self):
+        while self.computer_ship_count > 0 or self.player_ship_count > 0:
+            self.display()
+            
+            # First take user input
+            prow = int(input("Enter row: "))
+            pcol = int(input("Enter col: "))
+
+            # Check if theres a ship present. Handle accordingly
+            if self.player_grid[prow][pcol] == self.ship:
+                self.player_ship_count -= 1
+
+            self.player_grid[prow][pcol] = self.bomb
+
+            # Do the same for computer
+            crow = random.randint(0, self.size-1)
+            ccol = random.randint(0, self.size-1)
+
+            if self.computer_grid[crow][ccol] == self.ship:
+                self.computer_ship_count -= 1
+
+            self.computer_grid[crow][ccol] = self.bomb
 
     # Function to display the grid
     def display(self):
@@ -33,23 +79,17 @@ class Battleship:
     # Player's grid display function
     def display_player(self):
         print(f"{self.player_name}'s board: ")
-        
-        for i in range(len(self.player_grid)):
-            for j in range(len(self.player_grid[i])):
-                print(self.player_grid[i][j], end="   ")
-            print()
+        for row in self.player_grid:
+            print("   ".join(row))
+        print()
 
     # Computers's grid display function
     def display_computer(self):
         print("Computer's board: ")
-
-        for i in range(len(self.computer_grid)):
-            for j in range(len(self.computer_grid[i])):
-                if self.player_grid[i][j] == self.bomb:
-                    print(self.bomb, end="   ")
-                else:
-                    print(self.target, end="   ")
-            print()
+        for row in self.computer_grid:
+            display_row = [self.bomb if cell == self.bomb else self.target for cell in row]
+            print("   ".join(display_row))
+        print()
 
 if __name__ == "__main__":
     # Get custom grid size
@@ -58,4 +98,6 @@ if __name__ == "__main__":
 
     # Play game
     battleship = Battleship(grid_size, name)
-    battleship.display()
+    battleship.play()
+
+
