b34d01782255431c3b2d2fd49405c58a16a0463b
annabellaals <160537930+annabellaals@users.noreply.github.com>
2024-05-21 14:36:33 +0500

    v8
    Linter approved

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import random
-from typing import List, Tuple
+
 
 class Battleship:
     def __init__(self, size, name) -> None:
@@ -11,8 +11,10 @@ class Battleship:
         self.bomb = "X"
 
         self.size = size
-        self.player_grid = [[self.target for _ in range(size)] for _ in range(size)]  
-        self.computer_grid = [[self.target for _ in range(size)] for _ in range(size)]  
+        self.player_grid = [[self.target for _ in range(size)]
+                            for _ in range(size)]
+        self.computer_grid = [[self.target for _ in range(size)]
+                              for _ in range(size)]
 
         self.player_ship_count = 0
         self.computer_ship_count = 0
@@ -49,17 +51,18 @@ class Battleship:
         self.clear()
         pstr = "did" if phit else "did not"
         cstr = "did" if chit else "did not"
-        
+
         print(f"Round {self.round}: \n")
         print(f"---{self.player_name} attacked: ({pcords[0]}, {pcords[1]})")
         print(f"---{self.player_name} {pstr} hit a ship!\n")
         print(f"---Computer attacked: ({ccords[0]}, {ccords[1]})")
         print(f"---Computer {cstr} hit a ship!\n")
 
-        print(f"Player: {self.size - self.computer_ship_count - 1}\tComputer: {self.size - self.player_ship_count - 1}")
+        print(f"Player: {self.size - self.computer_ship_count - 1}\t"
+              f"Computer: {self.size - self.player_ship_count - 1}")
 
         input("\nPress enter to continue...")
-    
+
     def end_credits(self):
         self.clear()
         if self.player_ship_count == self.computer_ship_count:
@@ -73,27 +76,29 @@ class Battleship:
         while self.computer_ship_count > 0 and self.player_ship_count > 0:
             player_hit = computer_hit = False
             self.display()
-            
+
             while True:
                 try:
                     crow = int(input("Enter row: "))
                     ccol = int(input("Enter col: "))
                     if crow < 0 or crow >= self.size or ccol < 0 or ccol >= self.size:
-                        print(f"Coordinates out of bounds. Please enter values between 0 and {self.size - 1}.")
+                        print(f"Coordinates out of bounds. "
+                              f"Please enter values between 0 and {self.size - 1}.")
                         continue
                     if self.computer_grid[crow][ccol] == self.bomb:
-                        print("You've already bombed this location. Choose a different one.")
+                        print("You've already bombed this location. "
+                              "Choose a different one.")
                         continue
                     break
                 except ValueError:
                     print("Invalid input. Please enter numbers only.")
-            
+
             if self.computer_grid[crow][ccol] == self.ship:
                 self.computer_ship_count -= 1
                 computer_hit = True
 
             self.computer_grid[crow][ccol] = self.bomb
-            
+
             while True:
                 prow = random.randint(0, self.size-1)
                 pcol = random.randint(0, self.size-1)
@@ -107,10 +112,11 @@ class Battleship:
             self.player_grid[prow][pcol] = self.bomb
 
             self.round += 1
-            self.transmission(computer_hit, (crow, ccol), player_hit, (prow, pcol))
+            self.transmission(computer_hit, (crow, ccol),
+                              player_hit, (prow, pcol))
 
         self.end_credits()
-           
+
     def display(self):
         self.clear()
         self.display_player()
@@ -126,20 +132,22 @@ class Battleship:
     def display_computer(self):
         print("Computer's board: ")
         for row in self.computer_grid:
-            display_row = [self.bomb if cell == self.bomb else self.target for cell in row]
+            display_row = [self.bomb if cell == self.bomb
+                           else self.target for cell in row]
             print("   ".join(display_row))
         print()
+
 
 if __name__ == "__main__":
     choice = -1
 
     while choice != 0:
         print("------BELLA'S BATTLESHIP------\n")
-       
+
         while True:
             try:
                 grid_size = int(input("Enter grid size: "))
-                if grid_size >= 3 and grid_size <= 10:
+                if 3 <= grid_size <= 10:
                     break
                 else:
                     print("Invalid choice. Please enter between 3 or 10.")
